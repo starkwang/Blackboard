5990304cb642f12d94f7954dfdc0e4d9002fe05e
starkwang <381152119@qq.com>
2015-10-08 20:11:08 +0800

    add tabindent

--- static/js/index.js
+++ static/js/index.js
@@ -1,18 +1,20 @@
-  var socket = io.connect('http://localhost:8080');
-  socket.on('update', function(data) {
-      document.getElementById('input').value = data;
-      render();
-  });
+var socket = io.connect('http://192.168.1.100:8080');
+socket.on('update', function(data) {
+    document.getElementById('input').value = data;
+    render();
+});
 
-  function render() {
-      var value = document.getElementById('input').value;
-      document.getElementById('code').innerHTML = '<pre class="brush: js;">' + value + '</pre>';
-      SyntaxHighlighter.highlight();
-  }
+function render(value) {
+    document.getElementById('code').innerHTML = '<pre class="brush: xml;">' + value + '</pre>';
+    SyntaxHighlighter.highlight();
+}
 
-  function input() {
-      render();
-      var value = document.getElementById('input').value;
-      socket.emit('input', value);
-  }
-  SyntaxHighlighter.all();
+function input() {
+    var value = document.getElementById('input').value;
+    value = value.replace(/</ig, '&lt;').replace(/>/ig, '&gt;').replace(/"/ig, '&quot;');
+    render(value);
+
+    socket.emit('input', value);
+}
+SyntaxHighlighter.all();
+tabIndent.renderAll();
