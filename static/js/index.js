65bfa87ed5c0fe02c05a6639fa6878f3535a78c8
starkwang <381152119@qq.com>
2015-10-19 22:04:45 +0800

    fix bugs

--- static/js/index.js
+++ static/js/index.js
@@ -1,20 +1,23 @@
-var socket = io.connect('http://192.168.1.100:8080');
+var socket = io.connect('http://10.73.73.73:8193');
 socket.on('update', function(data) {
     document.getElementById('input').value = data;
-    render();
+    render(translate(data));
 });
 
+function translate(value){
+    return value.replace(/</ig, '&lt;').replace(/>/ig, '&gt;').replace(/"/ig, '&quot;');
+}
+
 function render(value) {
-    document.getElementById('code').innerHTML = '<pre class="brush: xml;">' + value + '</pre>';
+    document.getElementById('code').innerHTML = '<pre class="brush: js;">' + value + '</pre>';
     SyntaxHighlighter.highlight();
 }
 
 function input() {
     var value = document.getElementById('input').value;
-    value = value.replace(/</ig, '&lt;').replace(/>/ig, '&gt;').replace(/"/ig, '&quot;');
-    render(value);
+    render(translate(value));
 
-    socket.emit('input', value);
+    socket.emit('input', document.getElementById('input').value);
 }
 SyntaxHighlighter.all();
 tabIndent.renderAll();
